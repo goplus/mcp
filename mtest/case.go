8ca0135a6f05f1cf19dccae6c33be0a51b3caa36
xushiwei <x@goplus.org>
2025-04-10 07:44:09 +0800

    mcp server test: initialize

--- mtest/case.go
+++ mtest/case.go
@@ -36,7 +36,7 @@ type CaseApp struct {
 	ctx context.Context
 }
 
-// Req create a new request given method.
+// Req create a new request with given method.
 func (p *CaseApp) Req__0(method string) *Request {
 	p.Request = &Request{method: method, ctx: p}
 	return p.Request
@@ -47,20 +47,48 @@ func (p *CaseApp) Req__1() *Request {
 	return p.Request
 }
 
+// Initialize creates a new request to initialize the MCP server.
+func (p *CaseApp) Initialize(params map[string]any) *Request {
+	return p.Req__0("initialize").Params(mapJoin(map[string]any{
+		"protocolVersion": mcp.LATEST_PROTOCOL_VERSION,
+		"capabilities": map[string]any{
+			"roots":    map[string]any{"listChanged": true},
+			"sampling": map[string]any{},
+		},
+		"clientInfo": map[string]any{
+			"name":    "github.com/goplus/mcp/mtest",
+			"version": "0.3.0",
+		},
+	}, params))
+}
+
 // OnNotify registers a notification handler.
 func (p *CaseApp) OnNotify(notify func(method string, params map[string]any)) {
 	p.client.OnNotification(func(in mcp.JSONRPCNotification) {
-		notify(in.Method, makeParams(in.Params))
+		notify(in.Method, makeParams(in.Params.Meta, in.Params.AdditionalFields))
 	})
 }
 
-func makeParams(in mcp.NotificationParams) map[string]any {
-	if in.Meta == nil {
-		return in.AdditionalFields
+func makeParams(meta, addition map[string]any) map[string]any {
+	if meta == nil {
+		return addition
 	}
-	m := make(map[string]any, len(in.AdditionalFields)+1)
-	maps.Copy(m, in.AdditionalFields)
-	m["_meta"] = in.Meta
+	m := make(map[string]any, len(addition)+1)
+	maps.Copy(m, addition)
+	m["_meta"] = meta
+	return m
+}
+
+func mapJoin(a, b map[string]any) map[string]any {
+	if a == nil {
+		return b
+	}
+	if b == nil {
+		return a
+	}
+	m := make(map[string]any, len(a)+len(b))
+	maps.Copy(m, a)
+	maps.Copy(m, b)
 	return m
 }
 
@@ -78,6 +106,7 @@ func Gopt_CaseApp_TestMain(c iCaseProto, t *testing.T) {
 	app := new(App).initApp()
 	c.initCaseApp(app, test.NewT(t))
 	c.Main()
+	app.shutdown()
 }
 
 func (p *CaseApp) initCaseApp(app *App, t CaseT) {
